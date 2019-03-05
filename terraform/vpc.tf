52165bd250a85aa2531760ac4e8770ff1ad283cf
Anderson Farias <anderson_si@outlook.com>
2019-03-04 22:14:56 -0300

    Edit vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     enable_dns_support = "true"
-    enable_dns_hostname = "true"
+    enable_dns_hostnames = "true"
     enable_classiclink = "false"
     tags{
         Name = "main"
