205ca6186c08334a1e62b537cd046e5a90338355
Anderson Farias <anderson_si@outlook.com>
2019-03-04 22:27:32 -0300

    edit sg.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -3,7 +3,7 @@
 resource "aws_security_group" "access-remote" {
     name = "access-remote"
     vpc_id = "${aws_vpc.main.id}"
-    description "Libera acesso remote via SSH e RDP"
+    description = "Libera acesso remote via SSH e RDP"
     ingress {
        from_port = 22
        to_port = 22
@@ -24,7 +24,7 @@ resource "aws_security_group" "access-remote" {
 resource "aws_security_group" "access-web" {
     name = "access-web"
     vpc_id = "${aws_vpc.main.id}"
-    description "Libera acesso web"
+    description = "Libera acesso web"
     ingress {
        from_port = 80
        to_port = 80
