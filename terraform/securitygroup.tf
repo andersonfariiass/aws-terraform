7e62079c6dbef19eded53cf757534a47658dcab2
Anderson Farias <anderson_si@outlook.com>
2019-03-04 22:24:33 -0300

    Edit sg.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -20,3 +20,24 @@ resource "aws_security_group" "access-remote" {
         Name = "access-remote"
     }
 }
+
+resource "aws_security_group" "access-web" {
+    name = "access-web"
+    vpc_id = "${aws_vpc.main.id}"
+    description "Libera acesso web"
+    ingress {
+       from_port = 80
+       to_port = 80
+       protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags {
+        Name = "access-web"
+    }
+}
