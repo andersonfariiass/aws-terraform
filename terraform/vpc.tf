a85f6472c4499fad18effda8dccb743cbc44d133
Anderson Farias <anderson_si@outlook.com>
2019-03-04 22:52:32 -0300

    Edit vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -79,5 +79,6 @@ resource "aws_route_table" "main-public-rt" {
 
 resource "aws_route_table_association" "main-public-1a" {
     subnet_id = "${aws_subnet.main-public-1.id}"
+    subnet_id = "${aws_subnet.main-public-2.id}"
     route_table_id = "${aws_route_table.main-public-rt.id}"
 }
