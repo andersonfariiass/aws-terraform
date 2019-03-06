eb91a8770720480ac0a1d69b4c3b4d40b6cda170
Anderson Farias <anderson_si@outlook.com>
2019-03-06 20:38:41 -0300

    Edit instance.tf

--- terraform/instace.tf
+++ terraform/instace.tf
@@ -7,3 +7,12 @@ resource "aws_instance" "vm-cloud" {
     vpc_security_group_ids = ["${aws_security_group.access-remote.id}"]
     associate_public_ip_address = "true"
 }
+
+resource "aws_instance" "vm-cloud2" {
+    ami = "ami-0ac019f4fcb7cb7e6"
+    instance_type = "t2.micro"
+    key_name = "access-cloudjob"
+    subnet_id = "${aws_subnet.main-public-2.id}"
+    vpc_security_group_ids = ["${aws_security_group.access-remote.id}"]
+    associate_public_ip_address = "true"
+}
