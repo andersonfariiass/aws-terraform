d2ba3a3eaa855f62ea2b9bddbfb6fc6efa2adcdb
Anderson Farias <anderson_si@outlook.com>
2019-03-06 21:28:52 -0300

    Edit sg.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -16,6 +16,18 @@ resource "aws_security_group" "access-remote" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 8
+        to_port = 0
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     tags {
         Name = "access-remote"
     }
