916b3669945af9d696983fb81f0781deae78e7db
Anderson Farias <anderson_si@outlook.com>
2019-03-11 20:36:52 -0300

    Edit loadb.tf

--- terraform/loadb.tf
+++ terraform/loadb.tf
@@ -1,14 +1,14 @@
 #Create resource Load Balace
 
 resource "aws_elb" "loadb" {
-    name_prefix = "loadb"
+    name = "loadb"
     #availability_zones = ["us-east-1a", "us-east-1c"]
     subnets = ["${aws_subnet.main-public-1.id}", "${aws_subnet.main-public-2.id}"]
     health_check {
-        healthy_threshold = 4
-        unhealthy_threshold = 4
-        timeout = 3
-        target = "HTTP:80/"
+        healthy_threshold = 10
+        unhealthy_threshold = 2
+        timeout = 5
+        target = "HTTP:80/index.html"
         interval = 30
     }
 
@@ -21,6 +21,7 @@ resource "aws_elb" "loadb" {
     cross_zone_load_balancing = true
     instances = ["${aws_instance.vm-cloud.id}","${aws_instance.vm-cloud2.id}"]
     security_groups = ["${aws_security_group.access-web.id}"]
+    connection_draining = true
 }
 
 # Verificar por o lb não esta funcionando via terraform!
