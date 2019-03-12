ecbb4acad7497b59c5ca32eef57615a5a157b429
Anderson Farias <anderson_si@outlook.com>
2019-03-11 21:12:51 -0300

    Edit loadb.tf

--- terraform/loadb.tf
+++ terraform/loadb.tf
@@ -20,7 +20,7 @@ resource "aws_elb" "loadb" {
     }
     cross_zone_load_balancing = true
     instances = ["${aws_instance.vm-cloud.id}","${aws_instance.vm-cloud2.id}"]
-    security_groups = ["${aws_security_group.access-web.id}"]
+    security_groups = ["${aws_security_group.access-web.id}", "${aws_security_group.access-remote.id}"]
     connection_draining = true
 }
 
